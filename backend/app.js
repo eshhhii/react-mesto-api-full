c5fd2b30e6e5ca51efbe257bd57360d660873e53
Emma <emmashiller@gmail.com>
2021-11-07 16:22:23 +0200

    ref: changed back port on backend

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const {
 const { NotFound } = require("./errors/NotFound");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
